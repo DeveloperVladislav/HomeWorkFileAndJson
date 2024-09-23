05b741e34ec7824760abc4e1a2f26728cb1457ae
Vladislav <110498828+DeveloperVladislav@users.noreply.github.com>
2024-09-23 21:09:24 +0300

    Add project files.

---
+++ HomeWorkFile/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkFileAndJson
+{
+	public class Person
+	{
+		public static string path = @"D:\ДЗ.РепетиторствоC#\Текст\Text.txt";   // путь к файлу
+		public int Age { get; set; }
+		public string? Name { get; set; }
+
+		public Person(string name,int age)
+		{
+			Name = name;
+			Age = age;
+		}
+
+		public async Task WriteInFile()
+		{
+			string text = await ReadInFile();
+
+			text += $"{Name}, {Age}\n";
+			//1 способ через File
+			using (FileStream fstream = new FileStream(path, FileMode.Create))
+			{
+				byte[] buffer = Encoding.Default.GetBytes(text);
+				await fstream.WriteAsync(buffer, 0, buffer.Length);
+				Console.WriteLine("Данные добавлены в файл");
+			}
+			//2 способ через Stream
+			/*using (StreamWriter writer = new StreamWriter(path, true))
+			{
+				await writer.WriteLineAsync(text);
+				Console.WriteLine("Данные добавлены в файл");
+				
+			}*/
+
+		}
+
+		public async Task<string> ReadInFile()
+		{
+			string textFromFile = "";
+
+			// Чтение из файла
+			if (File.Exists(path))
+			{
+				//1 способ через File
+				using (FileStream fstream = File.OpenRead(path))
+				{
+					byte[] buffer = new byte[fstream.Length];
+					await fstream.ReadAsync(buffer, 0, buffer.Length);
+					textFromFile = Encoding.Default.GetString(buffer);
+				}
+				//2 способ через Stream
+				/*using (StreamReader reader = new StreamReader(path))
+				{
+					textFromFile = await reader.ReadToEndAsync();
+				}*/
+
+			}
+			return textFromFile;
+		}
+	}
+}
---
+++ HomeWorkFile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO.Compression;
+using System.Text;
+using System.Text.Json;
+
+namespace HomeWorkFileAndJson
+{
+	public class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var person = new Person("", 0); // Создаем пустой объект Person
+			string allData = await person.ReadInFile();
+			Console.WriteLine("Все данные в файле:\n" + allData);
+			while (true)
+			{
+				Console.WriteLine("Введите имя:");
+				string? name = Console.ReadLine();
+				Console.WriteLine("Введите возраст:");
+				int age = Convert.ToInt32(Console.ReadLine());
+				person = new Person(name, age);
+				await person.WriteInFile();
+
+				allData = await person.ReadInFile();
+				Console.WriteLine("Все данные в файле:\n" + allData);
+				Console.WriteLine();
+
+				string json = JsonSerializer.Serialize(person);
+				Console.WriteLine($"Сериализация объекта Person - {json}");
+				Console.WriteLine();
+				Person deserializedPerson = JsonSerializer.Deserialize<Person>(json);
+				Console.WriteLine($"Десериализация объекта Person - Имя: {deserializedPerson?.Name}, Возраст: {deserializedPerson?.Age}");
+				Console.WriteLine();
+			}
+
+		}
+	}
+}
